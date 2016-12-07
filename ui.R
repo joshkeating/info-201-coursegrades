17ce6e581a090b521fecf8856bc2cc562d1bf3c4
Josh Keating <josh.keating7@gmail.com>
2016-12-07 14:17:32 -0800

    commented code and added shiny

--- ui.R
+++ ui.R
@@ -1,31 +1,36 @@
-
+# Required libraries and packages
 library(shiny)
 library(plotly)
 library(dplyr)
 library(rCharts)
 
-# Remember to change this is you're not Josh
+# Sources the data from the /resources dir
 gpa <- read.csv("./resources/UWgpa.csv")
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
+  # Main title header
   titlePanel("Comparing Course Grades Over Time"),
   
     sidebarPanel(
-    
+      
+      # defines values for textInput widget 
       textInput("text", label = h3("Select a Course"), value = "CSE 142"),
       
+      # displays some help text for the user 
       helpText("Note: Syntax of class input should be abbreviation of ",
                "the class followed by the course number i.e INFO 200."),
       
       # Uncomment this line for debugging
       # verbatimTextOutput('value'),
       
+      # defines values for checkboxGroupInput widget
       checkboxGroupInput("checkGroup", label = h3("Select Quarter(s)"),
                          choices = c(unique(as.character(gpa$Quarter))),
                          selected = c("Autumn", "Winter")),
       
+      # displays some help text for the user 
       helpText("Note: Select quarter(s) to view grade distribution.")
       
       # Uncomment this line for debugging
@@ -33,6 +38,7 @@ shinyUI(fluidPage(
       
     ),
     
+    # plota the data in the reactive main panel
     mainPanel(
       plotlyOutput("plot")
     )
